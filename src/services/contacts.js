3e7244049c2c47948dc2606b920972e6a238eb52
Nastia Komisar <anastasiakomissar0302@gmail.com>
2025-08-17 19:41:47 +0100

    Added sorting

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,14 +1,20 @@
+import { SORT_ORDER } from '../constants/index.js';
 import { ContactsCollection } from '../db/models/contactModel.js';
 import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-export const getAllContacts = async ({page, perPage}) => {
+export const getAllContacts = async ({
+  page = 1, 
+  perPage = 10,
+  sortOrder = SORT_ORDER.ASC,
+  sortBy = 'name',
+}) => {
   const limit = perPage;
   const skip = (page-1)*perPage;
 
   const contactsQuery = ContactsCollection.find();
   const contactsCount = await ContactsCollection.find().merge(contactsQuery).countDocuments();
 
-  const contacts = await contactsQuery.skip(skip).limit(limit).exec();
+  const contacts = await contactsQuery.skip(skip).limit(limit).sort({ [sortBy]: sortOrder }).exec();
 
   const paginationData = calculatePaginationData(contactsCount, perPage, page);
 
