b84f1c093da72dcce41a63c0ebc64784d7815eb0
Nastia Komisar <anastasiakomissar0302@gmail.com>
2025-08-17 21:42:58 +0100

    Changed filter

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -13,6 +13,10 @@ export const getAllContacts = async ({
   const skip = (page-1)*perPage;
 
   const contactsQuery = ContactsCollection.find();
+
+  if (filter.contactType) {
+    contactsQuery.where('contactType').equals(filter.contactType);
+  }
   if(typeof filter.isFavourite === 'boolean'){
     contactsQuery.where('isFavourite').equals(filter.isFavourite);
   }
--- src/utils/parseFilterParams.js
+++ src/utils/parseFilterParams.js
@@ -1,14 +1,23 @@
+const parseType = (contactType) => {
+  if (typeof contactType !== 'string') return;
+
+  const types =  ['work', 'home', 'personal'];
+  if(types.includes(contactType)) return contactType;
+};
+
 const parseIsFavourite = (isFavourite) => {
   if (isFavourite === 'true') return true;
   if (isFavourite === 'false') return false;
 };
 
 export const parseFilterParams = (query) => {
-  const { isFavourite } = query;
+  const { contactType, isFavourite } = query;
 
+  const parsedType = parseType(contactType);
   const parsedIsFavourite = parseIsFavourite(isFavourite);
 
   return {
+    contactType: parsedType,
     isFavourite: parsedIsFavourite,
   };
 };
