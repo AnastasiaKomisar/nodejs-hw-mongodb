834f16c6e54cae3a1d8903ac98090e96a634fe1d
Nastia Komisar <anastasiakomissar0302@gmail.com>
2025-09-01 17:31:55 +0300

    Added /auth/logout

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
 import { registerUserSchema } from '../validation/auth.js';
 import { registerUserController } from '../controllers/auth.js';
 import { loginUserSchema} from '../validation/auth.js';
 import { loginUserController } from '../controllers/auth.js';
 import { refreshUserSessionController } from '../controllers/auth.js';
-import { validateBody } from '../middlewares/validateBody.js';
+import { logoutUserController } from '../controllers/auth.js';
+
 
 const router = Router();
 
@@ -24,6 +26,11 @@ router.post(
 router.post(
     '/refresh',
     ctrlWrapper(refreshUserSessionController),
+);
+
+router.post(
+    '/logout',
+    ctrlWrapper(logoutUserController),
 );
 
 export default router;
