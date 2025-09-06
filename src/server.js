4b950414131fb6738a872a9715c803a033fe8248
Nastia Komisar <anastasiakomissar0302@gmail.com>
2025-09-06 18:01:05 +0300

    Added the ability to upload photos

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import { getEnvVar } from './utils/getEnvVar.js';
 import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundhandler } from './middlewares/notFoundHandler.js';
+import { UPLOAD_DIR } from './constants/index.js';
 
 const logger = pino();
 
@@ -33,5 +34,7 @@ export function setupServer() {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
+
+  app.use('/uploads', express.static(UPLOAD_DIR));
 
 }
