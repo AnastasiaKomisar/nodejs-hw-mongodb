c45e638aab51cea15b337e16439fbd2047295985
Nastia Komisar <anastasiakomissar0302@gmail.com>
2025-09-14 19:02:11 +0300

    Fixed /api-docs rout 2

--- src/server.js
+++ src/server.js
@@ -26,14 +26,14 @@ export function setupServer() {
     next();
   });
 
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());  
+
   app.use(router);
 
   app.use(notFoundhandler);
 
   app.use(errorHandler);
-
-  app.use('/uploads', express.static(UPLOAD_DIR));
-  app.use('/api-docs', swaggerDocs());  
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
